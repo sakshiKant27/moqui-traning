d7472d81b9f6a8784bebe38fbbfcb18968419f33
SakshiKant <kantsakshi03@gmail.com>
2025-01-10 18:09:01 +0530

    Some Minute Changes

--- script/MoquiTrainingScript.groovy
+++ script/MoquiTrainingScript.groovy
@@ -12,16 +12,16 @@ def trainingPrice = context.trainingPrice
 def trainingDuration = context.trainingDuration
 
 
-// Explicitly generate a unique ID
-def trainingID = ec.entity.sequencedIdPrimary("MoquiTraining", null, null)
+// Explicitly generate a unique id
+def trainingId = ec.entity.sequencedIdPrimary("MoquiTraining", null, null)
 
-// Create the MoquiTraining entity record
-EntityValue trainingRecord = ec.entity.makeValue "moqui.training.MoquiTraining"
 
+EntityValue trainingRecord = ec.entity.makeValue ("moqui.training.MoquiTraining")
 
 trainingRecord.set("trainingId", trainingId) // Explicitly set trainingId
 trainingRecord.set("trainingName", trainingName)
 trainingRecord.set("trainingDate", trainingDate)
-
-// Set the output parameter
-context.trainingID = trainingRecord.get("trainingId")
+if(trainingPrice!=null) trainingRecord.set("trainingPrice",trainingPrice)
+if(trainingDate!=null) trainingRecord.set("trainingDuration",trainingDuration)
+trainingRecord=trainingRecord.create()
+context .trainingId = trainingRecord.get("trainingId")//set output parameter
